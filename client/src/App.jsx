7bcde31033ec72c56cd1e7bf308bd586156299b4
sabitsadiq <sabitsdq@gmail.com>
2024-11-14 23:18:06 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
+const socket = io.connect("http://localhost:3001");
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
+
+  useEffect(() => {
+    socket.on("Receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <input
+        placeholder="Room Number ..."
+        onChange={(e) => setRoom(e.target.value)}
+      />
+      <button onClick={joinRoom}>Join Room</button>
+
+      <input
+        placeholder="Message ..."
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Message:</h1>
+      {messageReceived}
+    </div>
+  );
 }
 
-export default App
+export default App;
