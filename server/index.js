cc2bb8d209d891e84520046b4c46aad1daea4dfc
sabitsadiq <sabitsdq@gmail.com>
2024-11-14 23:51:25 +0100

    fixes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const http = require("http");
-
-const { server } = require("socket.io");
+const { Server } = require("socket.io"); // Correct import for Server
 const cors = require("cors");
-const { Socket } = require("dgram");
+
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
+  // Renamed variable to avoid conflict
   cors: {
     origin: "http://localhost:5174",
     methods: ["GET", "POST"],
@@ -23,11 +23,12 @@ io.on("connection", (socket) => {
     console.log(data);
     socket.join(data);
   });
+
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("reciever_message", data);
   });
 });
 
 server.listen(3001, () => {
-  console.log("Server is runing");
+  console.log("Server is running");
 });
